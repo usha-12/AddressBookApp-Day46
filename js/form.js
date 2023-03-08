c2e4c7aa69207c7f84b5d3ccf6aba17ca91a9cfc
usha-12 <usha12ahirwar@gmail.com>
2023-03-08 21:28:12 +0530

    Added: view details from local storage

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+let addressBookContactJSONObject = {};
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     const nameOutput = document.querySelector('.text-error');
@@ -46,3 +47,86 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+const saveForm = () => {
+    try {
+        let addressBook = createAddressBook();
+        createAndUpdateStorage(addressBook);
+        resetForm();
+        window.location.replace(site_properties.home_page);
+    } catch (e) {
+        return
+    }
+}
+
+const createAddressBook = () => {
+    let addressBook = new AddressBookDetails();
+    try {
+        addressBook.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+    try {
+        addressBook.address = getInputValueById('#address');
+    } catch (e) {
+        setTextValue('.address-error', e);
+        throw e;
+    }
+    addressBook.city = document.getElementById("city").value;
+    addressBook.state = document.getElementById("state").value;
+    addressBook.zip = getInputValueById('#zip');
+    try {
+        addressBook.phoneNumber = getInputValueById('#phoneNo');
+    } catch (e) {
+        setTextValue('.phoneNo-output', e);
+        throw e;
+    }
+    alert(addressBook.toString());
+    return addressBook;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+function createAndUpdateStorage(addressBook) {
+    let addressBookList = JSON.parse(localStorage.getItem("AddressBookList"));
+    if (addressBookList != undefined) {
+        addressBookList.push(addressBook);
+    } else {
+        addressBookList = [addressBook];
+    }
+    alert(addressBookList.toString());
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+}
+
+const resetForm = () => {
+    setValue('#name', "");
+    setValue('#address', "");
+    setValue('#city', '');
+    setSelectedIndex('#state', );
+    setSelectedIndex('#zip', );
+    setValue('#phoneNo', "");
+}
+
+const setValue = (id, value) => {
+    const element = document.getElementById(id);
+    element.value = value;
+}
+
+const setSelectedIndex = (id, index) => {
+    const element = document.querySelector(id);
+    element.selectedIndex = index;
+};
+
+const setaddressBookJSONObject = () => {
+    addressBookContactJSONObject._name = getInputValueById('#name');
+    addressBookContactJSONObject._address = getInputValueById('#address');
+    addressBookContactJSONObject._city = getInputValueById('#city');
+    addressBookContactJSONObject._state = getInputValueById('#state');
+    addressBookContactJSONObject._zip = getInputValueById('#zip');
+    addressBookContactJSONObject._phoneNumber = getInputValueById('#phoneNo');
+    alert("Added Json Object : " + addressBookContactJSONObject._name);
+};
